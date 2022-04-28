b296b0c4aa19da9e7efd393aa4ea5a36cab985fd
who <artglad05@mail.ru>
2022-04-29 00:55:00 +0500

    new

---
+++ venv/Include/25.py
@@ -0,0 +1,31 @@
+import math
+
+
+count = 0
+for i in range(700001, 800000):
+    
+    b = 0
+
+    for j in range(2, int(math.sqrt(i)) + 1):
+        if i % j == 0:
+            b = round(i / j)
+            break
+
+    
+    if b == 0:
+        M = 0
+    else:
+        M = j + b
+
+    if M != 0 and M % 10 == 8:
+        count += 1
+        print(i, M)
+
+
+    if count == 5:
+        break
+
+
+
+
+
